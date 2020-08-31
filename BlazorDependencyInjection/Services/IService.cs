118bc7c4b7941ecb894c589d62b3ddc4ca4232aa
William Rivera <williamr@traceamerica.com>
2020-08-31 11:25:18 -0400

    Add project files.

---
+++ BlazorDependencyInjection/Services/IService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorDependencyInjection.Services
+{
+    public interface IService
+    {
+        string GetToken();
+    }
+
+    public interface ISingletonService : IService { }
+    public interface IScopedService : IService { }
+    public interface ITransientService : IService { }
+
+    public class SingletonService : ISingletonService
+    {
+        private readonly string _token;
+        public SingletonService()
+        {
+            _token = Guid.NewGuid().ToString();
+        }
+
+        public string GetToken() => _token;
+    }
+
+    public class ScopedService : IScopedService
+    {
+        private readonly string _token;
+        public ScopedService()
+        {
+            _token = Guid.NewGuid().ToString();
+        }
+
+        public string GetToken() => _token;
+    }
+
+    public class TransientService : ITransientService
+    {
+        private readonly string _token;
+        public TransientService()
+        {
+            _token = Guid.NewGuid().ToString();
+        }
+
+        public string GetToken() => _token;
+    }
+}
---
+++ BlazorDependencyInjection/Startup.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using BlazorDependencyInjection.Data;
+using BlazorDependencyInjection.Services;
+
+namespace BlazorDependencyInjection
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<ISingletonService, SingletonService>();
+            services.AddScoped<IScopedService, ScopedService>();
+            services.AddTransient<ITransientService, TransientService>();
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
